05d13c2c34f6abed8216e53dbe0d75f5eb2c38b1
Cristian Eulises Sanchez Ramirez <scristianeulises@gmail.com>
2024-12-09 17:13:55 -0400

    rotate cube working

--- src/main.cpp
+++ src/main.cpp
@@ -17,14 +17,19 @@
 #include "ShaderProgram.h"
 #include "Texture2D.h"
 
-
 // Global Variables
-const char* APP_TITLE = "Coordnate Systems";
+const char* APP_TITLE = "3D Cube Control";
 int gWindowWidth = 1024;
 int gWindowHeight = 768;
 GLFWwindow* gWindow = NULL;
 bool gWireframe = false;
 const std::string texture1 = "textures/wooden_crate.jpg";
+
+// Movement and rotation variables
+glm::vec3 cubePos = glm::vec3(0.0f, 0.0f, -10.0f); // Cube position
+glm::vec3 cubeRotation = glm::vec3(0.0f, 0.0f, 0.0f); // Rotation angles
+const float movementSpeed = 0.1f; // Speed of movement
+const float rotationSpeed = 2.0f; // Speed of rotation
 
 // Function prototypes
 void glfw_onKey(GLFWwindow* window, int key, int scancode, int action, int mode);
@@ -39,14 +44,13 @@ int main()
 {
 	if (!initOpenGL())
 	{
-		// An error occured
 		std::cerr << "GLFW initialization failed" << std::endl;
 		return -1;
 	}
 
-	// 1. Set up an array of vectex data for a cube with index buffer data
+	// 1. Set up an array of vertex data for a cube with index buffer data
 	GLfloat vertices[] = {
-		 // position		 // tex coords
+		// position		 // tex coords
 
 		// front face
 		-1.0f,  1.0f,  1.0f, 0.0f, 1.0f,
@@ -97,28 +101,21 @@ int main()
 		 1.0f, -1.0f, -1.0f, 1.0f, 0.0f,
 	};
 
-	// Cube position
-	glm::vec3 cubePos = glm::vec3(0.0f, 0.0f, -10.0f);
-
-	// 2. Set up buffers on the GPU
 	GLuint vbo, vao;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glGenBuffers(1, &vbo);					// Generate an empty vertex buffer on the GPU
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);		// "bind" or set as the current buffer we are working with
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);	// copy the data from CPU to GPU
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
 
-	glGenVertexArrays(1, &vao);				// Tell OpenGL to create new Vertex Array Object
-	glBindVertexArray(vao);					// Make it the current one
-
-	// Position attribute
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(0));
 	glEnableVertexAttribArray(0);
 
-	// Texture Coord attribute
 	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
 	glEnableVertexAttribArray(1);
 
-	glBindVertexArray(NULL);					// unbind to make sure other code doesn't change it
+	glBindVertexArray(NULL);
 
 	ShaderProgram shaderProgram;
 	shaderProgram.loadShaders("shaders/basic.vert", "shaders/basic.frag");
@@ -126,51 +123,28 @@ int main()
 	Texture2D texture;
 	texture.loadTexture(texture1, true);
 
-	double lastTime = glfwGetTime();
-	float cubeAngle = 0.0f;
-
-	// Create the projection matrix
 	glm::mat4 model;
 	glm::mat4 view;
 	glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)gWindowWidth / (float)gWindowHeight, 0.1f, 50.0f);
 
-	// Rendering loop
 	while (!glfwWindowShouldClose(gWindow))
 	{
 		showFPS(gWindow);
-
-		double currentTime = glfwGetTime();
-		double deltaTime = currentTime - lastTime;
-
-		// Poll for and process events
 		glfwPollEvents();
-
-		// Clear the screen
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
 		texture.bind(NULL);
 
-		
 		model = glm::mat4(1.0f);
 		view = glm::mat4(1.0f);
 
-		// Update the cube position and orientation.  Rotate first then translate
-		cubeAngle += static_cast<float>(deltaTime * 50.0f);
-		if (cubeAngle >= 360.0f) cubeAngle = 0.0f;
+		model = glm::translate(model, cubePos) *
+		        glm::rotate(model, glm::radians(cubeRotation.x), glm::vec3(1.0f, 0.0f, 0.0f)) *
+		        glm::rotate(model, glm::radians(cubeRotation.y), glm::vec3(0.0f, 1.0f, 0.0f)) *
+		        glm::rotate(model, glm::radians(cubeRotation.z), glm::vec3(0.0f, 0.0f, 1.0f));
 
-		// Rotates around the cube center
-		model = glm::translate(model, cubePos) * glm::rotate(model, glm::radians(cubeAngle), glm::vec3(0.0f, 1.0f, 0.0f));
-
-		// Create the View matrix
-		glm::vec3 camPos(0.0f, 0.0f, 0.0f);
-		glm::vec3 targetPos(0.0f, 0.0f, -1.0f);
-		glm::vec3 up(0.0f, 1.0f, 0.0f);
-		view = glm::lookAt(camPos, camPos + targetPos, up);
-
+		view = glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, -1.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 
 		shaderProgram.use();
-
-		// Pass the matrices to the shader
 		shaderProgram.setUniform("model", model);
 		shaderProgram.setUniform("view", view);
 		shaderProgram.setUniform("projection", projection);
@@ -179,31 +153,20 @@ int main()
 		glDrawArrays(GL_TRIANGLES, 0, 36);
 		glBindVertexArray(NULL);
 
-		// Swap front and back buffers
 		glfwSwapBuffers(gWindow);
-
-		lastTime = currentTime;
 	}
 
-	// Clean up
 	glDeleteVertexArrays(1, &vao);
 	glDeleteBuffers(1, &vbo);
-	
 	glfwTerminate();
 
 	return 0;
 }
 
-//-----------------------------------------------------------------------------
-// Initialize GLFW and OpenGL
-//-----------------------------------------------------------------------------
 bool initOpenGL()
 {
-	// Intialize GLFW 
-	// GLFW is configured.  Must be called before calling any GLFW functions
 	if (!glfwInit())
 	{
-		// An error occured
 		std::cerr << "GLFW initialization failed" << std::endl;
 		return false;
 	}
@@ -211,9 +174,8 @@ bool initOpenGL()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);	// forward compatible with newer versions of OpenGL as they become available but not backward compatible (it will not run on devices that do not support OpenGL 3.3
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-	// Create an OpenGL 3.3 core, forward compatible context window
 	gWindow = glfwCreateWindow(gWindowWidth, gWindowHeight, APP_TITLE, NULL, NULL);
 	if (gWindow == NULL)
 	{
@@ -222,10 +184,7 @@ bool initOpenGL()
 		return false;
 	}
 
-	// Make the window's context the current one
 	glfwMakeContextCurrent(gWindow);
-
-	// Initialize GLEW
 
 	glewExperimental = GL_TRUE;
 	if (glewInit() != GLEW_OK)
@@ -234,40 +193,48 @@ bool initOpenGL()
 		return false;
 	}
 
-	// Set the required callback functions
 	glfwSetKeyCallback(gWindow, glfw_onKey);
 	glfwSetFramebufferSizeCallback(gWindow, glfw_onFramebufferSize);
 
 	glClearColor(0.23f, 0.38f, 0.47f, 1.0f);
 
-	// Define the viewport dimensions
 	glViewport(0, 0, gWindowWidth, gWindowHeight);
 	glEnable(GL_DEPTH_TEST);
 
 	return true;
 }
 
-//-----------------------------------------------------------------------------
-// Is called whenever a key is pressed/released via GLFW
-//-----------------------------------------------------------------------------
 void glfw_onKey(GLFWwindow* window, int key, int scancode, int action, int mode)
 {
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, GL_TRUE);
-	
-	if (key == GLFW_KEY_F1 && action == GLFW_PRESS)
+
+	if (action == GLFW_PRESS || action == GLFW_REPEAT)
 	{
-		gWireframe = !gWireframe;
-		if (gWireframe)
-			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-		else
-			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		switch (key)
+		{
+		case GLFW_KEY_W:
+			cubeRotation.x -= rotationSpeed; // Rotate up
+			break;
+		case GLFW_KEY_S:
+			cubeRotation.x += rotationSpeed; // Rotate down
+			break;
+		case GLFW_KEY_A:
+			cubeRotation.y -= rotationSpeed; // Rotate left
+			break;
+		case GLFW_KEY_D:
+			cubeRotation.y += rotationSpeed; // Rotate right
+			break;
+		case GLFW_KEY_Q:
+			cubePos.z += movementSpeed; // Move closer
+			break;
+		case GLFW_KEY_E:
+			cubePos.z -= movementSpeed; // Move away
+			break;
+		}
 	}
 }
 
-//-----------------------------------------------------------------------------
-// Is called when the window is resized
-//-----------------------------------------------------------------------------
 void glfw_onFramebufferSize(GLFWwindow* window, int width, int height)
 {
 	gWindowWidth = width;
@@ -275,36 +242,29 @@ void glfw_onFramebufferSize(GLFWwindow* window, int width, int height)
 	glViewport(0, 0, gWindowWidth, gWindowHeight);
 }
 
-//-----------------------------------------------------------------------------
-// Code computes the average frames per second, and also the average time it takes
-// to render one frame.  These stats are appended to the window caption bar.
-//-----------------------------------------------------------------------------
 void showFPS(GLFWwindow* window)
 {
 	static double previousSeconds = 0.0;
 	static int frameCount = 0;
 	double elapsedSeconds;
-	double currentSeconds = glfwGetTime(); // returns number of seconds since GLFW started, as double float
+	double currentSeconds = glfwGetTime();
 
 	elapsedSeconds = currentSeconds - previousSeconds;
 
-	// Limit text updates to 4 times per second
 	if (elapsedSeconds > 0.25)
 	{
 		previousSeconds = currentSeconds;
 		double fps = (double)frameCount / elapsedSeconds;
 		double msPerFrame = 1000.0 / fps;
 
-		// The C++ way of setting the window title
 		std::ostringstream outs;
-		outs.precision(3);	// decimal places
+		outs.precision(3);
 		outs << std::fixed
-			<< APP_TITLE << "    "
-			<< "FPS: " << fps << "    "
-			<< "Frame Time: " << msPerFrame << " (ms)";
+		     << APP_TITLE << "    "
+		     << "FPS: " << fps << "    "
+		     << "Frame Time: " << msPerFrame << " (ms)";
 		glfwSetWindowTitle(window, outs.str().c_str());
 
-		// Reset for next average.
 		frameCount = 0;
 	}
 
